94d55679ac5921f849acff3343addb4eccc92d72
dakyumlee <oicrcutie@gmail.com>
2025-07-01 09:29:48 +0900

    modify button error && change login UI

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,13 +1,14 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-auth.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-storage.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-storage.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-analytics.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCkwvuHGFWvSVp5MYYseWnnDFoDp1gZOHw",
   authDomain: "lgk7186-5c97d.firebaseapp.com",
   projectId: "lgk7186-5c97d",
-  storageBucket: "lgk7186-5c97d.appspot.com",
+  storageBucket: "lgk7186-5c97d.firebasestorage.app",
   messagingSenderId: "21729978003",
   appId: "1:21729978003:web:458d5acf44cd57dd3c4f66",
   measurementId: "G-MSGJ8NX6RW"
@@ -18,3 +19,6 @@ const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const storage = getStorage(app);
+export const analytics = getAnalytics(app);
+
+console.log('Firebase 초기화 완료');
